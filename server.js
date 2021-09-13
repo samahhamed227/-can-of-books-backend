8d15bc4e18f0b69738d2d5783e1f1c75719fb03e
samahhamed227 <samahhamed227@gmail.com>
2021-09-13 13:54:52 +0300

    edit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongoose =require('mongoose');
 const MONGO_URL = process.env.MONGO_URL;
 const DB_NAME = process.env.DB_NAME;
 
-mongoose.connect(`${MONGO_URL}/${DB_NAME}`);
+mongoose.connect(MONGO_URL);
 
 app.get('/test', (request, response) => {
 
