8c16d7321bc148928ec6d5bd5d052e4adeb04611
samahhamed227 <samahhamed227@gmail.com>
2021-09-14 00:13:23 +0300

    lab12

--- controller /books.controller.js
+++ controller /books.controller.js
@@ -11,7 +11,36 @@ const getboooks = (request, response) => {
 
 };
 
+const createbook = (request, response) => {
+  const { title, description, status,email } = request.body;
 
+
+  const newbook = new bookmodel({
+    title, description, status,email
+  });
+
+  newbook.save();
+
+  response.json(newbook);
+
+}
+
+//delet
+
+const deletebook= (request, response) => {
+  console.log(request.params);
+  const bookId2 = request.params.books_Id;
+
+  bookmodel.deleteOne({_id: bookId2 }, (error, deletedData) => {
+
+    response.json(deletedData);
+  });
+ 
+
+}
+  
 module.exports = {
-  getboooks
+  getboooks,
+  createbook,
+  deletebook
 }
